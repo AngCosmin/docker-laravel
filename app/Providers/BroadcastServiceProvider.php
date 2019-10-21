bdae7b2698da1e68773afc35082a9b8a570943af
Costin Adrian Pislaru <adrian.pislaru@tremend.com>
2019-10-21 16:38:36 +0300

    fixed typo

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -14,7 +14,7 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Broadcast::routes(['middleware' => ['auth:api']]);
+        Broadcast::routes(['middleware' => ['auth:api']]);
 
         require base_path('routes/channels.php');
     }
