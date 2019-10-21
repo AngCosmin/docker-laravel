f63e6c4424b22c97a3f639a76164c26b227c7e7f
Costin Adrian Pislaru <adrian.pislaru@tremend.com>
2019-10-21 16:36:42 +0300

    fixed routes

--- routes/channels.php
+++ routes/channels.php
@@ -15,6 +15,6 @@ Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('test', function ($user) {
-    return $user;
+Broadcast::channel('test', function () {
+    return true;
 });
