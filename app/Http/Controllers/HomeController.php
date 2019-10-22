e0a7e0b44cfb60a8c284d218c05105d33bb29ee7
Costin Adrian Pislaru <adrian.pislaru@tremend.com>
2019-10-22 12:08:50 +0300

    fake event

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Events\TestEvent;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
+        event(new TestEvent());
+
         return view('home');
     }
 }
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -17,6 +17,7 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+            '../Events/TestEvent.php'
         ],
     ];
 
